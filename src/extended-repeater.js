53d34b3f1b38e943e875cba4612a492d999c4f9d
kirillank <kirill.ank04@yandex.by>
2021-03-18 21:28:05 +0300

    fix: fix code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  if(typeof str != 'string') {
+    str = String(str);
+  }
+
+  let result = [];
+  let newresult = [];
+
+  if(!options.separator) {
+    options.separator = "+";
+  }
+
+  if(options.hasOwnProperty("addition")) {
+    if(typeof options.addition != 'string') {
+      options.addition = String(options.addition);
+    }
+    if(!options.additionSeparator) {options.additionSeparator = "|";}
+    for(let i = 0; i < options.additionRepeatTimes - 1; i++) {
+      newresult.push(options.addition);
+      newresult.push(options.additionSeparator);
+    }
+    newresult.push(options.addition);
+    newresult.push(str);
+    str = newresult.reverse().join('');
+  }
+
+for(let j = 0; j < options.repeatTimes - 1; j++) {
+  result.push(str);
+  result.push(options.separator);
+}
+result.push(str);
   
+return result.join('');
+};
