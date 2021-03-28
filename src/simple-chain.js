aa5829f0a54c52d359f3683f801e3f7e2e954715
kirillank <kirill.ank04@yandex.by>
2021-03-28 22:13:16 +0300

    fix: fix code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
-
+let chainlet =[];
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chainlet.length();
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      value = ' '; 
+  }
+  chainlet.push(String(value));
+  return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position)) {
+      chainlet = [];
+      throw new Error('Error');
+    }
+  chainlet.splice(position - 1, 1);
+  return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chainlet.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chain = chainlet.join(" )~~( ");
+    chainlet.length = 0;
+    return '( ' + chain + ' )';
   }
 };
 
